5fe628b53ecad4bd5452f465452f464d011792bc
saqib iqbal <saqiboiqbal7772@gmail.com>
2024-01-14 20:54:24 +0500

    sum function

--- Project1/src/main.rs
+++ Project1/src/main.rs
@@ -1,4 +1,8 @@
 fn main() {
     println!("Hello, {firstperson} is {secondperson} world!",firstperson="saqib",secondperson="Tommy");
+    mssg1();
 }
 
+fn mssg1(){
+    print!(" Adding Number {one} + {two} = {sum} ",one=1,two=5,sum=6)
+}
